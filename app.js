1537a5df3e67fb0657799ea57bfdf1808e4e7ecd
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-02-04 20:40:48 +0100

    Update

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿const express = require('express');
+const bodyParser = require('body-parser')
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+let tasks =  [
+    {id: 1, description: 'Faire les courses'},
+    {id: 2, description: 'Faire à manger'}
+];
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+})
+
